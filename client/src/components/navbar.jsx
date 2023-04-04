d8afd1f7982701ab3bb61c6e5c681a870f627a9c
danazou <dana.sou@gmail.com>
2023-04-04 15:24:31 -0700

    set logout with redirection to login/register page

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,12 +1,25 @@
-import {Link} from "react-router-dom";
-
+import {Link, useNavigate} from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 export const Navbar = () => {
-    return (
+
+    const [cookies, setCookies] = useCookies(["access_token"]);
+    const navigate = useNavigate();
+
+    const logout = () => {
+        setCookies("access_token", ""); //setting access token to be an empty token
+        window.localStorage.removeItem("userID");    //clear userID in local storgage
+        navigate("/"); //caling navigate for rediraction after logout
+    }
+
+
+     return (
         <div className="navbar">
             <Link to="/"> Home </Link>
-            <Link to="/about"> About </Link>
-            <Link to="/auth"> Login/Register</Link>
-        </div>
-    )
-}
+            <Link to="/create-recipe"> Create Recipe </Link>
+            <Link to="/saved-recipes"> Saved Recipes </Link>
+            {!cookies.access_token ? (<Link to="/auth"> Login/Register</Link>): 
+            (<button onClick={logout}>Logout</button>)}
+    </div> 
+    );
+};
