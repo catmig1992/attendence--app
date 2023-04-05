a4ed32367f3e089368a021c5331793a7c7c11edb
Catherine Miguel <catmig1992@gmail.com>
2023-04-04 19:27:00 -0700

    added portal

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,24 +1,26 @@
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
 export const Navbar = () => {
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
 
-    const [cookies, setCookies] = useCookies(["access_token"]);
-    const navigate = useNavigate();
+  const logout = () => {
+    setCookies("access_token", ""); //setting access token to be an empty token
+    window.localStorage.removeItem("userID"); //clear userID in local storgage
+    navigate("/auth"); //caling navigate for redirectionto auth page(login/register) after logout
+  };
 
-    const logout = () => {
-        setCookies("access_token", ""); //setting access token to be an empty token
-        window.localStorage.removeItem("userID");    //clear userID in local storgage
-        navigate("/auth"); //caling navigate for redirectionto auth page(login/register) after logout
-    }
-
-    
-    return (
-        <div className="navbar">
-            <Link to="/"> Home </Link>
-            <Link to="/about"> About </Link>
-            {!cookies.access_token ? (<Link to="/auth"> Login/Register</Link>): 
-            (<button onClick={logout}>Logout</button>)}
-    </div> 
-    );
+  return (
+    <div className="navbar">
+      <Link to="/"> Home </Link>
+      <Link to="/about"> About </Link>
+      <Link to="/portal"> Portal </Link>
+      {!cookies.access_token ? (
+        <Link to="/auth"> Login/Register</Link>
+      ) : (
+        <button onClick={logout}>Logout</button>
+      )}
+    </div>
+  );
 };
