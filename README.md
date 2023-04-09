10cca34b783234fdbf9e5c90e3cb59898dbe712e
Catherine Miguel <catmig1992@gmail.com>
2023-04-09 13:35:07 -0700

    started ReadMe file - work in progress; started seed file - work in progress

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # attendence--app
+
+Timely
+
+The application that will help educators keep track of class attendance!
+
+Users are able to create an account, input class roster information and take attendance in an efficient way. 
+
+...to be continued...
+
+Please note, when forking this repository the following list of packages need to be installed:
+axios,
+bcrypt,
+cors,
+express,
+jsonwebtoken,
+mongoose,
+nodemon,
+react-cookie,
+react-router-dom
--- server/src/routes/createclass.js
+++ server/src/routes/createclass.js
@@ -13,7 +13,8 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+//route to create a class --add student names
+router.post("/portal", async (req, res) => {
   const createclass = new CreateClassModel(req.body);
   try {
     const response = await createclass.save();
@@ -23,4 +24,9 @@ router.post("/", async (req, res) => {
   }
 });
 
+//route for saving a class roster
+router.put("/portal", async (req, res) => {});
+
 export { router as createclassRouter };
+
+////adding notes for a test pull to  seee if i still get an error
